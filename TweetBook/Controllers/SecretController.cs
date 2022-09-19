ee4126363aad5d36cbecb5f020244fb3fbeefa1b
kelvinzap <chisom859@gmail.com>
2022-09-20 00:06:36 +0100

    Implemented ApiKey in the secretController

---
+++ TweetBook/Controllers/SecretController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TweetBook.Filter;
+
+namespace TweetBook.Controllers;
+
+public class SecretController : Controller
+{
+    [HttpGet("secret")]
+    [ApiKeyAuth]
+    public IActionResult GetSecret()
+    {
+        return Ok("I have No Secrets");
+    }
+}
